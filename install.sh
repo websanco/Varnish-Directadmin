c1365dfcb051800b68abd7acfd970497337e60cd
websanco <32726791+websanco@users.noreply.github.com>
2017-10-12 12:55:18 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@
 
 # This is scripts install Varnish Cache for Directadmin.
 ### Build by Jazz1611 ###
+
 RED="\033[01;31m"
 GREEN="\033[01;32m"
 RESET="\033[0m"
+
 #Clear Screen For Install
 clear
 
@@ -39,14 +41,14 @@ cd /etc/varnish/
 ip=$(( lynx --dump cpanel.net/showip.cgi ) 2>&1 | sed "s/ //g")
 sed -i "s#host = \"127.0.0.1\"#host = \"$ip\"#g" ./default.vcl
 cd /etc/httpd/conf/
-sed -i 's#Listen 80#Listen 8080#g' ./httpd.conf
+sed -i "s#Listen 80#Listen 8080#g" ./httpd.conf
 cd extra
 sed -i "s#:80#:8080#g" ./httpd-vhosts.conf
 cp -p /usr/local/directadmin/data/templates/virtual_host2.conf  /usr/local/directadmin/data/templates/custom/virtual_host2.conf
 cp -p /usr/local/directadmin/data/templates/virtual_host2_sub.conf  /usr/local/directadmin/data/templates/custom/virtual_host2_sub.conf
 cp -p  /usr/local/directadmin/data/templates/redirect_virtual_host.conf  /usr/local/directadmin/data/templates/custom/redirect_virtual_host.conf
 cd /usr/local/directadmin/data/templates/custom
-sed -i 's/<VirtualHost |IP|:|PORT_80| |MULTI_IP|>/<VirtualHost 127.0.0.1:8080 |IP|:8080 |MULTI_IP|>/g' *
+sed -i "s/<VirtualHost |IP|:|PORT_80| |MULTI_IP|>/<VirtualHost 127.0.0.1:8080 |IP|:8080 |MULTI_IP|>/g" *
 cd
 #wget http://www.stderr.net/apache/rpaf/download/mod_rpaf-0.6.tar.gz
 wget http://drupion.com/sites/default/files/mod_rpaf-0.6.tar_.gz
@@ -63,12 +65,15 @@ RPAFheader X-Forwarded-For
 </IfModule>" >> /etc/httpd/conf/extra/httpd-includes.conf
 echo "action=rewrite&value=httpd" >> /usr/local/directadmin/data/task.queue
 cd /etc/sysconfig/
-sed -i 's#VARNISH_LISTEN_PORT=6081#VARNISH_LISTEN_PORT=80#g' ./varnish
+sed -i "s#VARNISH_LISTEN_PORT=6081#VARNISH_LISTEN_PORT=80#g" ./varnish
+
 #Start Varnish Cache
 service httpd restart
 service varnish start
+
 #Start When Reboot
 chkconfig varnish on
+
 echo -e "$GREEN---------------------------------------------------$RESET"
 echo -e "$GREEN      Varnish Cache Install Completed       $RESET"
 echo -e "You can monitor varnish cache with command:$GREEN varnishstat $RESET"
