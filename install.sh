814cfaff061be7385f7e2c6896598c4bcfee406d
websanco <32726791+websanco@users.noreply.github.com>
2017-10-12 11:43:07 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,10 +3,6 @@
 # This is scripts install Varnish Cache for Directadmin.
 ### Build by Jazz1611 ###
 
-#RED='\033[01;31m'
-#GREEN='\033[01;32m'
-#RESET='\033[0m'
-
 #Clear Screen For Install
 clear
 
